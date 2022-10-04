e8316843678e66b1d45c0dffe78985a00674be5f
MissRosy <88013528+MissRosy@users.noreply.github.com>
2022-10-03 19:48:56 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, jsonify
+from model_prediction import *
+
+app = Flask(__name__)
+
+text=""
+predicted_emotion=""
+predicted_emotion_img_url=""
+
+#Renderizar la página HTML
+@app.route("/")
+def home():
+    entries = show_entry()
+    return render_template("index.html", entries=entries)
+    
+#Predecir emoción
+@app.route("/predict-emotion", methods=["POST"])
+def predict_emotion():
+    input_text = request.json.get("text")
+    if not input_text:
+        return jsonify({
+            "status": "error",
+            "message": "¡Por favor, ingresa algún texto para predecir la emoción!"
+        }), 400
+    else:
+        predicted_emotion, predicted_emotion_img_url = predict(input_text)                         
+        return jsonify({
+            "data": {
+                "predicted_emotion": predicted_emotion,
+                "predicted_emotion_img_url": predicted_emotion_img_url
+            },
+            "status": "success"
+        }), 200
+        
+#API O RUTA PARA GUARDAR ENTRADA
+
+
+    #Obtener la fecha, predecir emoción y texto ingresado por el usuario para guardar la entrada
+   
+           
+                
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
